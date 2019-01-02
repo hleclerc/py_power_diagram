07e2cf4e7fb30f5215a52548caa56061fdf22187
Hugo Leclerc <hugal.leclerc@gmail.com>
2019-01-02 09:44:41 +0100

    simple test

--- tests/test_get_measures.py
+++ tests/test_get_measures.py
@@ -6,24 +6,18 @@ import numpy as np
 domain = pd.domain_types.ConvexPolyhedraAssembly()
 domain.add_box( [ 0, 0 ], [ 1, 1 ] )
 
-np.random.seed( 5 )
-
 # diracs
 nb_diracs = 100
 positions = np.random.rand( nb_diracs, 2 )
 weights   = np.ones( nb_diracs )
 
-grid = pd.grid_types.ZGrid( 1000 )
-grid.update( positions, weights )
-grid.display_vtk( "vtk/grid.vtk" )
-
 # integrals
-areas = pd.integration( positions, weights, domain, grid )
+areas = pd.integration( positions, weights, domain )
 print( areas )
 print( np.sum( areas ) )
 
 # display
-pd.display_vtk( "vtk/pd.vtk", positions, weights, domain, grid )
+pd.display_vtk( "vtk/pd.vtk", positions, weights, domain )
 domain.display_boundaries_vtk( "vtk/domain.vtk" )
 
 # der measures
